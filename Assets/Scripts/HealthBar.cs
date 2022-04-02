aa9d720177c0a02402c9049d5d2e957a49ecadff
karina-ranadive <karinaranadive@gmail.com>
2022-04-02 16:40:27 -0400

    initial health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    public Health playerHealth;
+
+    private void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        healthBar = GetComponent<Slider>();
+        healthBar.maxValue = playerHealth.max;
+        healthBar.value = playerHealth.max;
+    }
+
+    public void SetHealth(int hp)
+    {
+        healthBar.value = hp;
+    }
+}
+
